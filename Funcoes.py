2bde63fe9ea83403e0543e456cb89cc4218678bf
andrespleite <andrespl@al.insper.edu.br>
2024-04-26 14:43:09 -0300

    funcao posicao suporta

--- Funcoes.py
+++ Funcoes.py
@@ -9,3 +9,28 @@ def cria_mapa(n):
         l.append(l2)
     
     return l
+
+##################################
+
+def posicao_suporta(mapa,b,l,c,o):
+    if mapa[l][c] == ' ':
+        if o == 'v':
+            for x in range(b):
+                if l + x >= (len(mapa)):
+                    return False
+                elif mapa[l+x][c] == ' ':
+                    continue
+                else:
+                    return False
+        else:
+            for x in range(b):
+                if c + x >= len(mapa[l]):
+                    return False
+                elif mapa[l][c+x] == ' ':
+                    continue
+                else:
+                    return False
+    else:
+        return False
+    
+    return True
