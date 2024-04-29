923966cb64a786fadfb05074b81a447dc0976b97
Gustavotfz <gustavo.tfernandez@gmail.com>
2024-04-29 11:38:33 -0300

    7 - Criar Mapas

--- Principal.py
+++ Principal.py
@@ -1,5 +1,84 @@
+#IMPORTS
 import random
 
+##########################################################################
+
+def cria_mapa(n):
+    l = []
+    l2 = []
+
+    for x in range(n):
+        l2.append(' ')
+    
+    for x in range(n):
+        l.append(l2)
+    
+    return l
+
+##########################################################################
+
+def posicao_suporta(mapa,b,l,c,o):
+    if mapa[l][c] == ' ':
+        if o == 'v':
+            for x in range(b):
+                if l + x >= (len(mapa)):
+                    return False
+                elif mapa[l+x][c] == ' ':
+                    continue
+                else:
+                    return False
+        else:
+            for x in range(b):
+                if c + x >= len(mapa[l]):
+                    return False
+                elif mapa[l][c+x] == ' ':
+                    continue
+                else:
+                    return False
+    else:
+        return False
+    
+    return True
+
+##########################################################################
+
+def aloca_navios (mapa, blocos):
+    pecas = 0
+    while pecas < len(blocos):
+        linha = random.randint(0, len(mapa)-1)
+        coluna = random.randint(0, len(mapa)-1)
+        orientacao = random.choice(['h', 'v'])
+        if posicao_suporta(mapa,blocos[pecas],linha,coluna,orientacao):
+            for i in range(blocos[pecas]):
+                if orientacao == "v":
+                    mapa[linha+i][coluna] = "N"
+                elif orientacao == "h":
+                    mapa[linha][coluna+i] = "N"
+            pecas += 1
+    return mapa
+
+##########################################################################
+
+def foi_derrotado (matriz):
+    for linha in matriz:
+        for item in linha:
+            if item == "N":
+                return False
+    return True
+
+##########################################################################
+
+# quantidade de blocos por modelo de navio
+CONFIGURACAO = {
+    'destroyer': 3,
+    'porta-avioes': 5,
+    'submarino': 2,
+    'torpedeiro': 3,
+    'cruzador': 2,
+    'couracado': 4
+}
+
+# frotas de cada pais
 PAISES =  {
     'Brasil': {
         'cruzador': 1,
@@ -38,6 +117,27 @@ PAISES =  {
     }
 }
 
+# alfabeto para montar o nome das colunas
+ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# cores para o terminal
+CORES = {
+    'reset': '\u001b[0m',
+    'red': '\u001b[31m',
+    'black': '\u001b[30m',
+    'green': '\u001b[32m',
+    'yellow': '\u001b[33m',
+    'blue': '\u001b[34m',
+    'magenta': '\u001b[35m',
+    'cyan': '\u001b[36m',
+    'white': '\u001b[37m'
+}
+
+##########################################################################
+
+
+
+
 game_status = True
 while game_status:
     titulo = " =====================================\n|                                     |\n| Bem-vindo ao INSPER - Batalha Naval |\n|                                     |\n =======   xxxxxxxxxxxxxxxxx   ======= \n"
@@ -47,3 +147,5 @@ while game_status:
     pais_computador = random.choice(list(PAISES.keys()))
     print("Computador está alocando os navios de batalhado país",pais_computador+"...")
     print("Computador já está em posição de batalha!")
+    mapa_computador = cria_mapa(10)
+    mapa_jogador = cria_mapa(10)
