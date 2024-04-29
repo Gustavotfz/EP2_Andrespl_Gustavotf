528a5265013619b5ed7ccf0821668893857e5917
Gustavotfz <gustavo.tfernandez@gmail.com>
2024-04-29 11:14:50 -0300

    Etapa 3 - Escolha do País do Computador

--- Principal.py
+++ Principal.py
@@ -1,7 +1,47 @@
+import random
+
+PAISES =  {
+    'Brasil': {
+        'cruzador': 1,
+        'torpedeiro': 2,
+        'destroyer': 1,
+        'couracado': 1,
+        'porta-avioes': 1
+    }, 
+    'França': {
+        'cruzador': 3, 
+        'porta-avioes': 1, 
+        'destroyer': 1, 
+        'submarino': 1, 
+        'couracado': 1
+    },
+    'Austrália': {
+        'couracado': 1,
+        'cruzador': 3, 
+        'submarino': 1,
+        'porta-avioes': 1, 
+        'torpedeiro': 1
+    },
+    'Rússia': {
+        'cruzador': 1,
+        'porta-avioes': 1,
+        'couracado': 2,
+        'destroyer': 1,
+        'submarino': 1
+    },
+    'Japão': {
+        'torpedeiro': 2,
+        'cruzador': 1,
+        'destroyer': 2,
+        'couracado': 1,
+        'submarino': 1
+    }
+}
+
 game_status = True
 while game_status:
     titulo = " =====================================\n|                                     |\n| Bem-vindo ao INSPER - Batalha Naval |\n|                                     |\n =======   xxxxxxxxxxxxxxxxx   ======= \n"
     msg_inicial = "Iniciando o Jogo!"
     print(titulo)
     print(msg_inicial)
-    break
+    pais_computador = random.choice(list(PAISES.keys()))
