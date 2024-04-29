0602c458482df00a63b7ca4ac1fe94f89874594d
Gustavotfz <gustavo.tfernandez@gmail.com>
2024-04-29 10:06:29 -0300

    Título + Mensagem Inicial

---
+++ Principal.py
@@ -0,0 +1,7 @@
+game_status = True
+while game_status:
+    titulo = " =====================================\n|                                     |\n| Bem-vindo ao INSPER - Batalha Naval |\n|                                     |\n =======   xxxxxxxxxxxxxxxxx   ======= \n"
+    msg_inicial = "Iniciando o Jogo!"
+    print(titulo)
+    print(msg_inicial)
+    break
