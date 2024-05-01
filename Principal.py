3cb096a2c50927d63d46bb65fc415246e4f26070
Gustavotfz <gustavo.tfernandez@gmail.com>
2024-04-30 23:43:05 -0300

    Underline nos prints ALOCAR e PRÓXIMOS

--- Principal.py
+++ Principal.py
@@ -63,9 +63,9 @@ while game_status:
                                
     for navio in PAISES[NUMERO_PAISES[escolha_pais]]:
         lista_navios.remove(navio)
-        print('ALOCAR:', navio, '(' + str(CONFIGURACAO[navio]) + ' blocos)')
+        print(CORES["underline"]+'ALOCAR:'+CORES["reset"], navio, '(' + str(CONFIGURACAO[navio]) + ' blocos)')
         navios_disponiveis = ", ".join(lista_navios)
-        print('PRÓXIMOS:', navios_disponiveis)
+        print(CORES["underline"]+'PRÓXIMOS:'+CORES["reset"], navios_disponiveis)
 
         escolha_letra = (input('\nInforme uma letra: ')).upper()
         while escolha_letra not in ALFABETO:
